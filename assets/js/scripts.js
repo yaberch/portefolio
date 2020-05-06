bd85b7165677cdcdd4fa111c098f0abdbc8cacce
yaberch <aberchihyoussef.ya@gmail.com>
2020-05-07 01:06:44 +0200

    enfin

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,16 +40,6 @@ jQuery(function ($) {
     // Animated scrolling / Scroll Up
     // -------------------------------------------------------------
 
-    (function () {
-        $('a[href*=#]').bind("click", function(e){
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
-            }, 1000);
-            e.preventDefault();
-        });
-    }());
-
 
 
     // -------------------------------------------------------------
